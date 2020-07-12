92f03bef522df71d12a6280b6eb26c5409bd512a
Randy Ortiz <Randyortiz28@gmail.com>
2020-07-12 18:55:00 -0400

    added files to master branch

---
+++ js/index.js
@@ -0,0 +1,12 @@
+
+function grayText() {
+document.querySelector('.searchtext').style.backgroundColor = "gray";
+}
+function hover() {
+document.querySelector('.searchtext').style.backgroundColor = "white";
+}
+
+function nameVisible(){
+document.querySelector('.Name').style.opacity = "1";
+}
+nameVisible();
